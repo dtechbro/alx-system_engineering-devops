5f4cdb114d3983cfb3a7b38e7cd5526af230a92a
Developer-in-Tech <princeakinola05@gmail.com>
2023-05-26 13:23:26 +0100

    file'

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 """export todo list of all employees in JSON"""
-from collections import UserString
 import json
 import requests
 
@@ -10,10 +8,11 @@ if __name__ == "__main__":
     user = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({u.get("id"): [{
-            "task": t.get("title"),
-            "completed": t.get("completed"),
-            "username": u.get("username")
-        } for t in requests.get(url + "todos",
-                params={"userId": u.get("id")}).json()]
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
             for u in users}, jsonfile)
